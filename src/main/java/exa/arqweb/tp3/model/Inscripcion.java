f7362517c8ebeb294cc8d33e405fb27e13799cec
pedrochatelain <pedrochatelain@gmail.com>
2023-10-02 12:36:55 -0300

    add relacion entre carrera y estudiante

---
+++ src/main/java/exa/arqweb/tp3/model/Inscripcion.java
@@ -0,0 +1,59 @@
+package exa.arqweb.tp3.model;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "inscripcion")
+public class Inscripcion {
+
+    @EmbeddedId
+    private InscripcionKey id;
+
+    @ManyToOne
+    @MapsId("libreta_universitaria")
+    @JoinColumn(name = "id_estudiante")
+    private Estudiante estudiante;
+
+    @ManyToOne
+    @MapsId("id")
+    @JoinColumn(name = "id_carrera")
+    private Carrera carrera;
+
+    @Column
+    private Integer anio_inscripcion;
+
+    @Column
+    private Integer anio_graduacion;
+
+    public Inscripcion() {}
+
+    public Inscripcion(Estudiante e, Carrera c, int anio_inscripcion) {
+        this.id = new InscripcionKey(e.getLibreta_universitaria(), c.getId());
+        this.estudiante = e;
+        this.carrera = c;
+        this.anio_inscripcion = anio_inscripcion;
+    }
+
+    public Integer getAnioInscripcion() {
+        return anio_inscripcion;
+    }
+
+    public void setAnioGraduacion(Integer anio_graduacion) {
+        this.anio_graduacion = anio_graduacion;
+    }
+
+    @Embeddable
+    private class InscripcionKey implements Serializable {
+        @Column(name = "id_estudiante")
+        private int idEstudiante;
+        @Column(name = "id_carrera")
+        private int idCarrera;
+        public InscripcionKey() {}
+        public InscripcionKey(int idEstudiante, int idCarrera) {
+            this.idEstudiante = idEstudiante;
+            this.idCarrera = idCarrera;
+        }
+    }
+
+}
